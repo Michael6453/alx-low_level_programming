2947608374808769c1db2c92aec720871d3e33ed
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-13 18:57:12 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 *
 *Return: By default returns zero if no errors.
 */
-int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,\n");
+int main()
+        { printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
