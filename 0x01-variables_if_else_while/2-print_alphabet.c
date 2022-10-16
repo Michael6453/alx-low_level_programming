7c3800f48e60b6c6aa15525f01f9e4e24e9ee359
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-15 17:07:44 -0700

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/** main - Starts the program
+/**
+ * main - Starts the program
  * return: returns zero
  */
 int main(void)
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
+putchar("ch\n");
 return (0);
 }
