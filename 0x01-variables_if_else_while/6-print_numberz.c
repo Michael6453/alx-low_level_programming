ddf16eeb5dd0f8f4d0290f75cbfb1a443793acb1
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-16 02:22:39 -0700

    Task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Starts the program
- * Return: returns zero
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar(num);
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
