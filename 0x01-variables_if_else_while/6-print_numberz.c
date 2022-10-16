5c5073019301b6bfeacac998573b0dd2acabd983
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-16 00:47:12 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Starts the program
+ * Return: returns zero
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
