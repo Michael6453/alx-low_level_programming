fdb3b90b9b593a0f50eeb0b35f220495501c20e2
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-19 13:43:06 -0700

    start

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
- * main - starts the program
- * Return: returns zero
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-int i;
-for (i = 97; i <= 122; i++)
-putchar(i);
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	print_alphabet();
+	return (0);
 }
