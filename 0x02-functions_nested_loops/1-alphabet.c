11ee6a1f89d0e5540f1d8ac25afebcab88841657
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-19 19:05:14 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints letters
+ * 
+ * Return: Returns zero
+ */
+
+void print_alphabet(void)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
