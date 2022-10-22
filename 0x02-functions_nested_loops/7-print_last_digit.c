042816e292cf3d91357a1da17a8f3418b9ba2e8b
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-22 14:07:32 -0700

    Task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: the given number
+ * Return: returns the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int n;
+	int x;
+
+	if (n <)
+	n = -n;
+	x = n % 10;
+	return (x);
+}
