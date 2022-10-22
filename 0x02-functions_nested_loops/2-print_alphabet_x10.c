e620de51857c16927d65df9dc95ac6d44c2099c3
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-22 01:09:14 -0700

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints letters
+ *
+ * Return: Returns zero
+ */                                                                                                 void print_alphabet_x10(void)
+{
+int j;
+int x;
+for (x = 0; x <= 9; x++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
