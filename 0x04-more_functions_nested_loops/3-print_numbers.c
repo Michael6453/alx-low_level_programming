bfd2ee49a62addae0bc077a4caeb43806bb15944
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-23 03:23:16 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ * Return: returns the numbers
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; && n < 58; n++)
+		_putchar('n');
+	_putchar('\n');
+}
