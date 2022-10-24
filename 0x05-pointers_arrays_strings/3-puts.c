05494295c800ac4b2e73f4174a155be35f2fb857
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-24 16:17:46 -0700

    Task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,16 @@
-#include "main.h" 
-  
- /** 
-  * _puts - prints a string 
-  * @str: the string 
-  * 
-  * Return: the length of the string 
-  */ 
- void _puts(char *str) 
- { 
-         int i = 0; 
-  
-         while (str[i] != '\0') 
-         { 
-                 _putchar(str[i]); 
-                 i++; 
-         } 
-         _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+ * _puts - a function that puts
+ * @str: a parameter to _puts function
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0' ; str++)
+	{
+		 _putchar(*str);
+	}
+	_putchar('\n');
+}
