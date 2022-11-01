3c4349f2590a009a31697667df668cd5406e9f23
Michael Ogbe <michaelogbe61@gmail.com>
2022-10-31 20:05:21 -0700

    Task1 hot fixing

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - memory copy
+ * _memcopy - memory copy
  * @dest: is destination memory
  * @src: is source memory
  * @n: number of bytes to be copied
@@ -10,7 +10,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unasigned int a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 		dest[a] = src[a];
