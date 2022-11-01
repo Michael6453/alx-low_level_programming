916472b59c512af802c3a3da2c726a6d792a171e
Michael Ogbe <michaelogbe61@gmail.com>
2022-11-01 10:04:52 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
  * @s: the string to search
  * @c: the character to be located
- * Return: a pointer or a NULL
+ * Return: a pointer to the char or a NULL
  */
 
 char *_strchr(char *s, char c)
